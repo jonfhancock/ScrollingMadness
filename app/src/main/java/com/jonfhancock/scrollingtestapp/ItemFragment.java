6454bafb290ac4dbfa84271238fc72ae59f54812
Jon Hancock <jonfhancock@gmail.com>
2015-04-30 17:33:39 -0700

    initial commit

---
+++ app/src/main/java/com/jonfhancock/scrollingtestapp/ItemFragment.java
@@ -0,0 +1,191 @@
+package com.jonfhancock.scrollingtestapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ItemFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ItemFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ItemFragment extends Fragment {
+    private static final String ARG_POSITION = "ARG_POSITION";
+
+    private int mPosition;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private ItemAdapter mItemAdapter;
+
+    private OnFragmentInteractionListener mListener;
+
+    public static ItemFragment newInstance(int position) {
+        ItemFragment fragment = new ItemFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_POSITION, position);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mPosition = getArguments().getInt(ARG_POSITION);
+        }
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mItemAdapter = new ItemAdapter(getActivity());
+        mItemAdapter.setListener(new ItemAdapter.OnImageClickListner() {
+            @Override
+            public void onImageClicked(String url) {
+                mListener.onFragmentInteraction(url);
+            }
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root =  inflater.inflate(R.layout.fragment_item, container, false);
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+        return root;
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mItemAdapter);
+    }
+
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(String uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(String uri);
+    }
+
+    public static class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder>{
+        private List<RecyclerItem> mItems;
+        private Picasso mPicasso;
+        private LayoutInflater mInflater;
+        private OnImageClickListner mListener;
+
+        public void setListener(OnImageClickListner listener) {
+            mListener = listener;
+        }
+
+        public interface OnImageClickListner{
+            public void onImageClicked(String url);
+        }
+
+        public ItemAdapter(Activity activity) {
+            mItems = new ArrayList<>();
+            mInflater = activity.getLayoutInflater();
+            mPicasso = Picasso.with(activity);
+            for(int i = 0;i< new Random().nextInt(20);i++){
+                mItems.add(new RecyclerItem(i));
+            }
+        }
+
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View root = mInflater.inflate(R.layout.item_card, parent, false);
+            ItemViewHolder holder = new ItemViewHolder(root);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            mPicasso.load(mItems.get(position).thumbUrl).placeholder(R.drawable.conent_placeholder).into(holder.image);
+            holder.image.setTag(mItems.get(position).thumbUrl);
+            holder.image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mListener != null) {
+                        String url = (String) v.getTag();
+                        mListener.onImageClicked(url);
+                    }
+
+                }
+            });
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+    }
+    public static class ItemViewHolder extends RecyclerView.ViewHolder{
+        ViewGroup root;
+        ImageView image;
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            root = (ViewGroup) itemView;
+            image = (ImageView) root.findViewById(R.id.image);
+        }
+    }
+
+    public static  class RecyclerItem {
+        public RecyclerItem(int position) {
+            this.position = position;
+            this.thumbUrl = "http://lorempixel.com/1280/720/?"+position;
+        }
+
+        public int position;
+        public String thumbUrl;
+
+    }
+}
---
+++ app/src/main/java/com/jonfhancock/scrollingtestapp/MainActivity.java
@@ -0,0 +1,203 @@
+package com.jonfhancock.scrollingtestapp;
+
+import android.animation.ArgbEvaluator;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends ActionBarActivity implements ItemFragment.OnFragmentInteractionListener {
+
+    private ImageView mHeroImage;
+
+
+    private Toolbar mToolbar;
+    private View mHeader;
+    private SlidingTabLayout mIndicator;
+    private ViewPager mPager;
+    private ItemPagerAdapter mPagerAdapter;
+    private FrameLayout mTouchInterceptor;
+    private ViewGroup mContainer;
+    private View mContent;
+    private ArgbEvaluator mColorEvaluator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContent = getWindow().findViewById(Window.ID_ANDROID_CONTENT);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mHeader = findViewById(R.id.header);
+        mHeroImage = (ImageView) findViewById(R.id.hero_image);
+        mIndicator = (SlidingTabLayout) findViewById(R.id.indicator);
+        mPager = (ViewPager) findViewById(R.id.view_pager);
+        mTouchInterceptor = (FrameLayout) findViewById(R.id.touch_intercepter);
+        mContainer = (ViewGroup) findViewById(R.id.container);
+        mPagerAdapter = new ItemPagerAdapter(getSupportFragmentManager());
+
+        mPager.setAdapter(mPagerAdapter);
+        mIndicator.setViewPager(mPager);
+        Picasso mPicasso = Picasso.with(this);
+        mPicasso.load("http://lorempixel.com/1280/500/?1").placeholder(R.drawable.hero_placeholder).into(mHeroImage);
+
+        final GestureDetectorCompat detector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        mColorEvaluator = new ArgbEvaluator();
+
+        mTouchInterceptor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                return mContainer.dispatchTouchEvent(event) && detector.onTouchEvent(event) ;
+            }
+        });
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            startTy = (int) mIndicator.getTranslationY();
+            startRootViewHeight = mContainer.getHeight();
+            return true;
+        }
+
+        int startTy;
+        int startRootViewHeight;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG,"onFling velX = " + velocityX + " vely = " + velocityY);
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            if (Math.abs(distanceX) > Math.abs(distanceY)) {
+                return super.onScroll(e1,e2,distanceX,distanceY);
+            }
+
+            int scroll_diff = (int) (e1.getY() - e2.getY());
+            int toolbarTranslationY = 0;
+            int headerheight;
+            int toolbarTranslucent = Color.parseColor("#55FFFFFF");
+            int toolbarColor = Color.parseColor("#55FFFFFF");
+            int finalToolbarColor = getResources().getColor(android.R.color.holo_blue_dark);
+            boolean movingUp = scroll_diff > 0;
+            int targetTranslationY = (int) (startTy - scroll_diff);
+
+            if (movingUp) {
+                if (-targetTranslationY <= mHeader.getHeight()) {
+                    headerheight = startRootViewHeight - targetTranslationY;
+                    if (-targetTranslationY >= mHeader.getHeight() - mToolbar.getHeight()) {
+                        toolbarTranslationY = mHeader.getHeight() + targetTranslationY - mToolbar.getHeight();
+                        toolbarColor = finalToolbarColor;
+
+                    }else{
+                        float colorPercentage = 1.1f-(mHeader.getHeight()+ (float)targetTranslationY - mToolbar.getHeight() )/mIndicator.getHeight();
+                        if(colorPercentage < 0){
+                            toolbarColor = toolbarTranslucent;
+                        }else if(colorPercentage <=1){
+                           toolbarColor = (Integer) mColorEvaluator.evaluate(colorPercentage, toolbarTranslucent,finalToolbarColor);
+                        }else{
+                            toolbarColor = finalToolbarColor;
+                        }
+                    }
+
+
+                } else {
+                    targetTranslationY = -mHeader.getHeight();
+                    toolbarTranslationY = targetTranslationY;
+                    headerheight = mContent.getHeight() + mHeader.getHeight();
+                    toolbarColor = finalToolbarColor;
+                }
+
+            } else {
+                //moving down
+                if (targetTranslationY <= 0) {
+                    headerheight = startRootViewHeight - targetTranslationY;
+                    if (mHeader.getHeight() + targetTranslationY <= mToolbar.getHeight()) {
+                        toolbarTranslationY = mHeader.getHeight() + targetTranslationY - mToolbar.getHeight();
+                        toolbarColor = finalToolbarColor;
+
+                    }else{
+                        float colorPercentage = 1.1f-(mHeader.getHeight()+ (float)targetTranslationY - mToolbar.getHeight() )/mIndicator.getHeight();
+                        if(colorPercentage < 0){
+                            toolbarColor = toolbarTranslucent;
+                        }else if(colorPercentage <=1){
+                            toolbarColor = (Integer) mColorEvaluator.evaluate(colorPercentage, toolbarTranslucent,finalToolbarColor);
+                        }else{
+                            toolbarColor = finalToolbarColor;
+                        }
+                    }
+                } else {
+                    targetTranslationY = 0;
+                    toolbarColor = toolbarTranslucent;
+                    headerheight = ViewGroup.LayoutParams.MATCH_PARENT;
+                }
+            }
+            updateViews(targetTranslationY, toolbarTranslationY, headerheight,toolbarColor);
+
+            return true;
+        }
+    }
+
+    private void updateViews(int targetTranslationY, int toolbarTranslationY, int headerheight,int toolbarColor) {
+        mToolbar.setTranslationY(toolbarTranslationY);
+        mToolbar.setBackgroundColor(toolbarColor);
+
+        mIndicator.setTranslationY(targetTranslationY);
+        mHeader.setTranslationY(targetTranslationY);
+        mPager.setTranslationY(targetTranslationY);
+        ViewGroup.LayoutParams rootParams = mContainer.getLayoutParams();
+        rootParams.height = headerheight;
+        mContainer.setLayoutParams(rootParams);
+    }
+
+
+    @Override
+    public void onFragmentInteraction(String uri) {
+        Toast.makeText(this, "on clicked " + uri, Toast.LENGTH_SHORT).show();
+    }
+
+    public class ItemPagerAdapter extends CacheFragmentStatePagerAdapter {
+        private int count = 10;
+
+        public ItemPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        protected Fragment createItem(int position) {
+            return ItemFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Item " + position;
+        }
+    }
+}
